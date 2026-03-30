5a1640409f2d2b51260e2480ad021124e9770da7
Manj0tBenipal <manjotbenipal00001@gmail.com>
2026-03-29 22:46:30 -0500

    version change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dinorun-v2';
+const CACHE = 'dinorun-v3';
 const ASSETS = [
   './', './index.html', './style.css', './game.js', './manifest.json',
   './sounds/player-jump.mp3', './sounds/ai-jump.mp3', './sounds/dead.mp3'
